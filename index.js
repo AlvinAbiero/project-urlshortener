b2e2bc5c47cfc0920ce459bc59534b04e1bdef2e
AlvinAbiero <abieroalvin@gmail.com>
2025-04-03 14:12:51 +0300

    feat: implemented body parser

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dns = require("node:dns");
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+let urlDatabase = {}; // This will hold the short URL mappings
+let shortUrlCounter = 1; // Counter to generate short URLs
+
 app.use(cors());
+app.use(bodyParser.json());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+// POST route to create a short URL
+app.post("/api/shorturl", (req, res) => {
+  const originalUrl = req.body.url;
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
